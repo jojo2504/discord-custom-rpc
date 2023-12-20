64bcf9a2c4a8b19a1fb61c1ee49c2e3471b72fbf
jojo2504 <jojo.duchuy@gmail.com>
2023-12-20 14:22:31 +0100

    add: config template

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-application_ID = "1147641171582136380"
+application_ID = ""
 
-USERNAME = "jojo2504"
+USERNAME = ""
 
-button_1_text = "My LeetCode Account"
-button_1_url = "https://leetcode.com/jojo2504/"
+button_1_text = ""
+button_1_url = ""
 
-button_2_text = "My Github Account"
-button_2_url = "https://github.com/jojo2504"
+button_2_text = ""
+button_2_url = ""
