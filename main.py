44c3717965bcc0a22b6603a39eccec36f9828a36
jojo2504 <jojo.duchuy@gmail.com>
2023-12-20 14:17:10 +0100

    alpha 0.0.1

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import requests
 import json
 import time
 import pywinauto.application
+import _ctypes
 from threading import Thread
 from pypresence import Presence
 from datetime import timedelta
 from win32gui import GetForegroundWindow
 from win32process import GetWindowThreadProcessId
-from id_file import application_ID
+from config import *
 
 class Clock():
     @staticmethod
@@ -40,31 +41,47 @@ class ActiveTab(Thread):
                 dlg = app.top_window()
                 url = dlg.child_window(title="Address and search bar", control_type="Edit").get_value()
 
-                print(url)
-                if "leetcode.com/problems" in url:
-                    self.active_tab = url.split('/')[2].replace('-', ' ').title()
+                if "leetcode.com" in url:
+                    url = url.split('/')
+                    if url[2] == '':
+                        self.active_tab = "In The Menu"
+                    else:
+                        self.active_tab = url[2].replace('-', ' ').title()
                 else:
                     self.active_tab = "Idling"
-            except pywinauto.findwindows.ElementNotFoundError:
-                self.active_tab = "Idling"""
-        
-class API():
+                    
+            except (pywinauto.findwindows.ElementNotFoundError, RuntimeError, IndexError, _ctypes.COMError) as e:
+                print(e.args)
+                self.active_tab = "Idling"
+
+class API(Thread):
     def __init__(self) -> None:
-        self.request = requests.get("https://leetcode-stats-api.herokuapp.com/jojo2504")
+        Thread.__init__(self, daemon=True)
+        self.request = requests.get("https://leetcode-stats-api.herokuapp.com/{}".format(USERNAME))
         self.content = self.convertJson(self.request.content)
     
+    def run(self):
+        self.fetch_data()
+
     def convertJson(self, content):
         json_string = content.decode('utf-8')
         return json.loads(json_string)
     
-    def __print__(self):
-        print(self.content)
+    def fetch_data(self):
+        while True:
+            self.request = requests.get("https://leetcode-stats-api.herokuapp.com/{}".format(USERNAME))
+            self.content = self.convertJson(self.request.content)
+            time.sleep(10)
+
+    def __str__(self):
+        return str(self.request.status_code)
 
 class Application():
     def __init__(self, ID) -> None:
         self.ID = ID
         self.RPC = Presence(self.ID)
         self.infos = API()
+        self.infos.start()
 
         self.active_tab = ActiveTab()
         self.active_tab.start()
@@ -72,27 +89,27 @@ class Application():
         self.seconds = 0
 
     def connect(self):
-        print("Connecting.")
         self.RPC.connect()
 
     def update(self):
-        print("updating...")
-        self.seconds += 1
-        self.RPC.update(
-            details = "{} | Problems solved: {}".format(self.active_tab.active_tab, self.infos.content["totalSolved"]),
-            state = "{} elapsed".format(Clock.format_seconds(self.seconds)),
-            large_image = "leetcode_logo",
-            large_text = "LeetCode",
-            buttons = [
-                {"label": "My LeetCode Account", "url": "https://leetcode.com/jojo2504/"}, 
-                {"label": "My Github Account", "url": "https://github.com/jojo2504"}
-            ],
-        )
+        try:
+            self.seconds += 1
+            self.RPC.update(
+                details = "{} | Problems solved: {}".format(self.active_tab.active_tab, self.infos.content["totalSolved"]),
+                state = "{} elapsed".format(Clock.format_seconds(self.seconds)),
+                large_image = "leetcode_logo",
+                large_text = "LeetCode",
+                buttons = [
+                    {"label": button_1_text, "url": button_1_url}, 
+                    {"label": button_2_text, "url": button_2_url}
+                ],
+            )
+        except KeyError as e:
+            print(e.args)
 
 def main():
     application = Application(application_ID)
     application.connect()
-    #application.infos.__print__()
 
     while True:
         time.sleep(1)
